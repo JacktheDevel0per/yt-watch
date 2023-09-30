32c4cd2250237c0a963589c1fcac3907b37bdc59
Jack <jack@lemonlime.tech>
2023-09-29 23:16:42 -0700

    add first cmt

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function SearchBar() {
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div className="container FlickrCtrl">
+        <ion-header-bar className="bar-dark"> </ion-header-bar>
+
+        <div id="search-bar">
+          <div className="item item-input-inset">
+            <label className="item-input-wrapper" id="search-input">
+              <i className="icon ion-search placeholder-icon"></i>
+              <form className="row" id="greet-form">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  ng-model="query"
+                  ng-change="search()"
+                />
+              </form>
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SearchBar;
