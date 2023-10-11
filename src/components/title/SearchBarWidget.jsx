63a0017fb9c96e25bd928512bebfa16d9ceef557
Jack <jack@lemonlime.tech>
2023-10-11 16:34:07 -0700

    small update to search, remove red outline ring from Input component

--- src/components/title/SearchBarWidget.jsx
+++ src/components/title/SearchBarWidget.jsx
@@ -7,7 +7,7 @@ import SearchBarWiget from "./SearchBarWidget";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-import { Search } from "lucide-react";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { useSearchParams } from "react-router-dom";
 
 function SearchBarWidget() {
@@ -19,8 +19,14 @@ function SearchBarWidget() {
 
   return (
     <>
+      <div className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors h-10 w-10">
+        <MagnifyingGlassIcon className="h-5 w-5" />
+      </div>
+      {/* <Button variant="ghost" size="icon"></Button> */}
+      <Input placeholder="Search YouTube" className=" w-6/12 flex-grow" />
+
       {/* <Search className="h-5 w-5" /> */}
-      <div
+      {/* <div
         className="self-center flex-grow
       border bg-accent text-accent-foreground
       rounded-md
@@ -47,7 +53,7 @@ function SearchBarWidget() {
         font-medium
           flex h-10 w-full rounded-md bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
         />
-      </div>
+      </div> */}
     </>
   );
 }
